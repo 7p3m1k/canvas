4ca71220e698ffd495a202eef044cb77a82392be
7p3m1k <kor0885@gmail.com>
2021-02-08 23:53:16 +0900

    2021.02.08

---
+++ src/pages/canvas/canvas_02_08.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  onMount(()=> {
+    let myCanvas = document.getElementById("myCanvas");
+    let ctx = myCanvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(75, 25);
+    ctx.quadraticCurveTo(25, 25, 25, 62.5);
+    ctx.quadraticCurveTo(25, 100, 50, 100);
+    ctx.quadraticCurveTo(50, 120, 30, 125);
+    ctx.quadraticCurveTo(60, 120, 65, 100);
+    ctx.quadraticCurveTo(125, 100, 125, 62.5);
+    ctx.quadraticCurveTo(125, 25, 75, 25);
+    ctx.stroke();
+    })
+  
+  </script>
+  
+  <h2 class="title">말풍선 그리기 (Bezier and quadratic curves)</h2>
+  <span class="date">2021-02-08</span>
+  <p class="description">
+    quadraticCurveTo(cp1x, cp1y, x, y) <br/>
+    bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
+  </p>
+  
+  <canvas id="myCanvas" width="1000" height="600"></canvas>
+  
+  
+  <style>
+      @import url("./canvasText.css");
+  </style>
+  
