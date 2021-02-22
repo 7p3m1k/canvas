f6cbd12e77794735600e40b5dc680e513176a5d9
7p3m1k <kor0885@gmail.com>
2021-02-22 23:02:49 +0900

    02-22

---
+++ src/pages/canvas/canvas_02_22.svelte
@@ -0,0 +1,123 @@
+<script>
+  import { onMount } from 'svelte';
+
+
+  onMount(()=> {
+    let myCanvas = document.getElementById("myCanvas");
+    let ctx = myCanvas.getContext('2d');
+
+    const radian = Math.PI / 180 ;
+
+    const radius = 150;
+    const rotateDegree = 32;
+
+    const rectWidth  = radius;
+    const rectHeight = radius / 6;
+    const rectMargin = radius / 12;
+
+    //빨간색 반원
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(
+      radius * 3,   //가로 450
+      radius * 2,   //세로 300
+      radius,
+      // 0 * radian,  //각도를 넣을때는 라디안을 곱해줘야함  태극기는 32도정도 기울어져 있어서 rotateDegree = 32추가
+      rotateDegree * radian,
+      (180 + rotateDegree)* radian,
+      true,
+    )
+    ctx.fill();
+
+    //파란색 반원
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(
+      radius * 3,   //가로 450
+      radius * 2,   //세로 300
+      radius,
+      rotateDegree * radian, 
+      (180 + rotateDegree)* radian,
+      false,
+    )
+    ctx.fill();
+
+    //빨간색 작은원
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(
+      387, //radius로 계산해서 해야하는데 모르겠다..
+      259,
+      radius /2 ,
+      rotateDegree * radian, 
+      (180 + rotateDegree)* radian,
+      false,
+    )
+    ctx.fill();
+
+    //파란색 작은원
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(
+      512.5,  //radius로 계산해서 해야하는데 모르겠다..
+      341.5,
+      radius /2 ,
+      rotateDegree * radian, 
+      (180 + rotateDegree)* radian,
+      true,
+    )
+    ctx.fill();
+
+    //보조선
+    ctx.beginPath();
+    ctx.arc(
+      radius * 3,
+      radius * 2,
+      radius + radius * ( 1 / 2 ),
+      0 * radian,
+      360 * radian,
+      true
+    )
+    ctx.stroke();
+
+    //건 부분
+    ctx.resetTransform();
+    ctx.translate(300,110);
+    ctx.rotate(90);
+    ctx.fillStyle='black';
+    ctx.beginPath();
+    ctx.fillRect(0, 0, radius, radius / 6);
+
+    ctx.beginPath();
+    ctx.fillRect(0, rectHeight + rectMargin , rectWidth, rectHeight);
+
+    ctx.beginPath();
+    ctx.fillRect(0, rectHeight * 2 + rectMargin * 2 , rectWidth, rectHeight);
+  })
+</script>
+
+
+<h2 class="title">태극마크 ver2 (색넣기)</h2>
+<span class="date">2021-02-19</span>
+<p class="description">
+32도 기울어진 반지름을 이용하여 만들었다. 원안에 원을 넣어서 반을 나눈 느낌으루...<br/>
+
+const radian = Math.PI / 180 ;<br/>
+<br/>
+const radius = 150;<br/>
+const rotateDegree = 32;<br/>
+radian,radius,rotateDegree 미리 선언해주면 사용하기 편하다.
+</p>
+
+
+<canvas id="myCanvas" width="900" height="600"></canvas>
+
+
+<style>
+  @import url("./canvasText.css");
+
+  canvas {
+    background-color: #eee;
+    border: 1px solid black;
+  }
+</style>
---
+++ src/pages/webClone/Lullaby/Header.svelte
@@ -0,0 +1,82 @@
+<script>
+
+  let current ;
+
+</script>
+
+
+<div class="header">
+  <div class="header-wrapper">
+    <div class="logo">Maden</div>
+    <ul>
+      <li on:mouseover={() => current = 'Lookbook'}>Lookbook</li>
+      <li on:mouseover={() => current = 'Shop'}>Shop</li>
+      <li on:mouseover={() => current = ''}>Notice</li>
+      <li on:mouseover={() => current = ''}>About us</li>
+    </ul>
+    <div>
+      <span>Cart</span>
+      <span>Login</span>
+    </div>
+  </div>
+</div>
+
+{#if current === 'Lookbook'}
+<ul>
+  <li>2020</li>
+  <li>2019</li>
+  <li>2018</li>
+</ul>
+{/if}
+
+{#if current === 'Shop'}
+<ul>
+  <li>top</li>
+  <li>bottom</li>
+  <li>shoes</li>
+  <li>bag</li>
+</ul>
+{/if}
+
+
+<style lang="scss">
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99;
+    background-color: rgba(255,255,255,0.9);
+    border-bottom: 1px solid black;
+
+    .header-wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 15px;
+      height: 90px;
+
+      .logo {
+        font-family: Gilroy;
+        font-size: 36px;
+        font-weight: bold;
+        cursor: pointer;
+      }
+
+      li {
+        float: left;
+        padding: 0 18px;
+        cursor: pointer;
+      }
+
+      li:hover {
+        color: gray;
+      }
+
+      span {
+        padding: 0 6px;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
